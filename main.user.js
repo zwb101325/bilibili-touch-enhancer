52982233c20c617636825a4209c94591a40c70f9
zwb101325 <208409030+zwb101325@users.noreply.github.com>
2026-04-23 11:33:41 +0800

    Update UserScript version to 1.9.0

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili-touch-enhancer
 // @namespace    http://tampermonkey.net/
-// @version      1.8.9
+// @version      1.9.0
 // @description  单击显示/隐藏控制栏，双击播放/暂停，长按倍速播放，左右滑动调节播放进度，上下滑动调节亮度/音量
 // @author       You
 // @match        *://*.bilibili.com/*
@@ -286,7 +286,7 @@
         if (!progressBar) return;
 
         const barRect = progressBar.getBoundingClientRect();
-        const x = barRect.left + barRect.width * (ratio + 0.005);
+        const x = barRect.left + barRect.width * ratio;
         const y = barRect.top + barRect.height / 2;
 
         return { progressBar, x, y };
@@ -312,7 +312,7 @@
         prevX = clientX;
         video.currentTime = startVal;
 
-        const point = getProgressPoint(videoArea, startVal / video.duration);
+        const point = getProgressPoint(videoArea, (startVal+3) / video.duration);
         if (point) sendMouseEvent(point.progressBar, "mousemove", point.x, point.y);
         const previewTime = videoArea.querySelector(".bpx-player-progress-preview-time");
         if (previewTime) previewTime.textContent = formatTime(startVal);
